44c67bc454d5acf16a8ff28f1abcbecda3b845f3
Pierre-Louis ROUSSEAUX <pl74rou@gmail.com>
2023-10-25 22:29:24 +0200

    add first part with first and second page

---
+++ index.js
@@ -0,0 +1,11 @@
+function goToSecondPage(){
+    document
+    .querySelector("#secondPage")
+    .scrollIntoView({ behavior: "smooth", block: "start" });
+}
+
+function goToThirdPage(){
+    document
+    .querySelector("#thirdPage")
+    .scrollIntoView({ behavior: "smooth", block: "start" });
+}
