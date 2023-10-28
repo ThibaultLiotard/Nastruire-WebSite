36d8a16787fffefeb5090155998c68ecb5e13cd7
Pierre-Louis ROUSSEAUX <pl74rou@gmail.com>
2023-10-28 07:57:06 +0200

    add fivesPage

--- index.js
+++ index.js
@@ -15,3 +15,23 @@ function goToFourthPage(){
     .querySelector("#fourthPage")
     .scrollIntoView({ behavior: "smooth", block: "start" });
 }
+
+function goToFivesPage(){
+    document
+    .querySelector("#fivesPage")
+    .scrollIntoView({ behavior: "smooth", block: "start" });
+}
+
+function move_to_rigth(){
+    const carouselContainer = document.querySelector(".carousel-container");
+    const groupPresentation = document.querySelectorAll(".group_presentation");
+    carouselContainer.style.transition = "transform 0.5s";
+    carouselContainer.style.transform = `translateX(-100vw)`;
+}
+
+function move_to_left(){
+    const carouselContainer = document.querySelector(".carousel-container");
+    const groupPresentation = document.querySelectorAll(".group_presentation");
+    carouselContainer.style.transition = "transform 0.5s";
+    carouselContainer.style.transform = `translateX(0vw)`;
+}
