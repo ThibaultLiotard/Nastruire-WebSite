d4ace279f71ce0e940213c0f5253a91b89bc562d
Pierre-Louis ROUSSEAUX <pl74rou@gmail.com>
2023-12-29 13:08:05 +0100

    add burgerMenu

--- src/index.js
+++ src/index.js
@@ -3,3 +3,43 @@ function changeSection(sectionName) {
         .querySelector(`.${sectionName}`)
         .scrollIntoView({ behavior: "smooth", block: "start" });
 }
+
+function showBurgerMenu() {
+    document.querySelector("header").style.display = "block";
+    document.querySelector('header').style.animation = "menuBurgerArrived 1.5s"
+    document.querySelector('#burgerMenuIcon').style.animation = "departEffect 1.5s"
+    setTimeout(() => {
+        document.querySelector("#burgerMenuIcon").style.display = "none";
+    }, 1500);
+}
+
+function hideBurgerMenu() {
+    document.querySelector("#burgerMenuIcon").style.display = "block";
+    document.querySelector('header').style.animation = "menuBurgerDepart 1.5s"
+    document.querySelector('#burgerMenuIcon').style.animation = "arrivedEffect 1.5s"
+    setTimeout(() => {
+        document.querySelector("header").style.display = "none";
+    }, 1000);
+}
+
+function clickOnSection(sectionName){
+    hideBurgerMenu()
+    changeSection(sectionName)
+}
+
+// document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+// function ctrlShiftKey(e, keyCode) {
+//   return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+// }
+
+// document.onkeydown = (e) => {
+//   if (
+//     event.keyCode === 123 ||
+//     ctrlShiftKey(e, 'I') ||
+//     ctrlShiftKey(e, 'J') ||
+//     ctrlShiftKey(e, 'C') ||
+//     (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+//   )
+//     return false;
+// };
